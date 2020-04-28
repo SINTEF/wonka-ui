41b305328cf870d91b06dbc6370959981d21bda1
Jan Inge Husby <Jan.Inge.Husby@sintef.no>
2020-04-28 13:30:46 +0200

    CONNECT-154 add reporters: ['spec', 'kjhtml']

--- projects/application/karma.conf.js
+++ projects/application/karma.conf.js
@@ -20,6 +20,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
+    reporters: ['spec', 'kjhtml'],
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
