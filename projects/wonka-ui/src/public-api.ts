bbfc8d9bd1b66936cb87e7fe47a42e985476fa2a
Lasse Bjermeland <lasse.bjermeland@marintek.sintef.no>
2020-06-25 12:17:00 +0200

    Fix: Card is not exported

--- projects/wonka-ui/src/public-api.ts
+++ projects/wonka-ui/src/public-api.ts
@@ -6,6 +6,7 @@ export * from './lib/charts/chart.module';
 export * from './lib/containers/lists/list.module';
 export * from './lib/containers/key-value-unit-lists/key-value-unit-list.module';
 export * from './lib/containers/tables/table.module';
+export * from './lib/card/card.module';
 export * from './lib/forms/form.module';
 export * from './lib/info/info-panel.module';
 export * from './lib/navigation/navigation-bar/navigation-bar.module';
