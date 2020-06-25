af99fe9c948e9b6cb2657e5b484413c4cad6c28e
Lasse Bjermeland <lasse.bjermeland@marintek.sintef.no>
2020-06-25 14:46:05 +0200

    Still no luck

--- projects/wonka-ui/src/lib/card/card.component.ts
+++ projects/wonka-ui/src/lib/card/card.component.ts
@@ -1,5 +1,30 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Card, Action } from './card';
+import { Component, Input } from '@angular/core';
+
+export interface Card<T> {
+  input?: T;
+  imageUrl?: string;
+  title: string;
+  description: string;
+  content: string;
+  actions?: Action[];
+}
+
+export class Action {
+
+  myMethod: () => void;
+  title: string;
+
+  constructor(title: string, myMethod: () => void) {
+    this.title = title;
+    this.myMethod = myMethod;
+  }
+
+  run(): void {
+    this.myMethod();
+  }
+}
+
+
 @Component({
   selector: 'lib-app-card',
   templateUrl: './card.component.html',
--- projects/wonka-ui/src/lib/card/card.module.ts
+++ projects/wonka-ui/src/lib/card/card.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardComponent } from './card.component';
+import { CardComponent} from './card.component';
 
 @NgModule({
   declarations: [
